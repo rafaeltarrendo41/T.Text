3113237fc0b47a59ba1f3b59dbbe53e11c7ba567
rafaeltarrendo41 <a89162@alunos.uminho.pt>
2020-12-29 19:50:54 +0000

    1

--- src/cliente/TText.java
+++ src/cliente/TText.java
@@ -17,7 +17,7 @@ public class TText {
     public static void main(String[] args) {
         // TODO code application logic here
        // Sistema s = new Sistema();
-        Login l = new Login();
+        Login2 l = new Login2();
         l.setVisible(true);
     }
     
